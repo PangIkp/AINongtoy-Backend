013bbacddb4bd52adcedafea4f90bbcfd6278d2a
Drymartini2199 <a.sreesunthorn@gmail.com>
2025-03-14 17:10:36 +0700

    remove commented-out address route from server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const user = require('./routes/user');
 const auth = require('./routes/auth');
-const address = require('./routes/address'); // Add this line
+// const address = require('./routes/address'); // Add this line
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use("/api/v1/user", user);
 app.use("/api/v1/auth", auth);
-app.use("/api/v1/address", address); // Add this line
+// app.use("/api/v1/address", address); // Add this line
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
