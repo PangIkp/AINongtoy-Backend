8695645e03b8a458aaf91196a4ef17b8f82729ab
Rangtude <panggylovely@gmail.com>
2025-04-03 15:52:04 +0700

    Add delete user functionality for admin with authorization and error handling

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getUsers, createUser, updateUserProfile, deleteUser, getUserProfile, getAllUsersForAdmin,updateUserForAdmin } = require("../controllers/user");
+const { getUsers, createUser, updateUserProfile, deleteUser, getUserProfile, getAllUsersForAdmin,updateUserForAdmin, deleteUserForAdmin } = require("../controllers/user");
 const { protect, authorize } = require("../middleware/auth");
 
 const jwt = require("jsonwebtoken");
@@ -17,7 +17,7 @@ router.delete("/:id", deleteUser);
 // for Admin
 router.get("/admin/users", protect, authorize("admin"), getAllUsersForAdmin);
 router.patch("/admin/users/:id", protect, authorize("admin"), updateUserForAdmin);
-
+router.delete("/admin/users/:id", protect, authorize("admin"), deleteUserForAdmin);
 
 
 module.exports = router;
